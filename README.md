b4c15c79b10736114620fe67634da1dff53f697a
mohiqudraty <mohi.qudraty@gmail.com>
2023-11-29 12:05:40 +0600

    project deploy on firebase and localhost link replace

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# React + Vite
+# Dev Point
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+#### Live LInk:  https://dev-point-380a2.web.app/
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+### some project features here!
+
+-- user can post any ideas with post
+-- user can comment of another user posts
+-- if user register on this site he/se will get silver badge
+-- user can by plan/subscription
+-- user can not post more then 5 if user not buy any plan/subscription
+-- user can report of any user comment 
+-- admin can delete a user if he got a report on comment
+-- user can see all post without login but cannot post , comment, or cannot give vote without login/sign up.
+and more feature have like online forum website .
--- src/Hooks/useAxios/useAxiosSecure.jsx
+++ src/Hooks/useAxios/useAxiosSecure.jsx
@@ -3,43 +3,45 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "../useAuth/useAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://y-liart-pi.vercel.app",
 });
 
 const useAxiosSecure = () => {
-  const {logoutUser} = useAuth()
-  const navigate = useNavigate()
-  
+  const { logoutUser } = useAuth();
+  const navigate = useNavigate();
 
-  axiosSecure.interceptors.request.use(function(config){
-    const token = localStorage.getItem('access-token')
-    console.log('stoop by interceptor',token);
-    config.headers.authorization = `Bearer ${token}`
-    return config;
-  }, function(error){
-    // if error  -----
-    return Promise.reject(error)
-  })
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("access-token");
+      console.log("stoop by interceptor", token);
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      // if error  -----
+      return Promise.reject(error);
+    }
+  );
 
-// intercepts 401 and 403 status ---
-axiosSecure.interceptors.response.use(function(response){
-  return response
-}, async (error) =>{
-  const status = error.response.status;
-  console.log('status error', status);
+  // intercepts 401 and 403 status ---
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error", status);
 
-if(status === 401 || status === 403){
- await logoutUser()
-  navigate('/login')
-}
+      if (status === 401 || status === 403) {
+        await logoutUser();
+        navigate("/login");
+      }
 
-
-  return Promise.reject(error)
-})
-
+      return Promise.reject(error);
+    }
+  );
 
   return axiosSecure;
-
 };
 
 export default useAxiosSecure;
